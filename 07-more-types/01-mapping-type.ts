b42cf80e9e7af963292d4d242188531328070d93
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-03-09 12:42:53 +0000

    types

--- 07-more-types/01-mapping-type.ts
+++ 07-more-types/01-mapping-type.ts
@@ -1,6 +1,49 @@
 {
   interface Options {
-    material: string;
-    backlight: string;
+    subtitles: boolean;
+    volume: number;
   }
+
+  interface ReadOnlyOptions {
+    readonly subtitles: boolean;
+    readonly volume: number;
+  }
+
+  interface OptionalOptions {
+    subtitles?: boolean;
+    volume?: number;
+  }
+
+  interface NullableOptions {
+    subtitle: boolean | null;
+    volume: number | null;
+  }
+}
+
+{
+  interface Options {
+    subtitles: boolean;
+    volume: number;
+    brightness: number;
+  }
+
+  type ReadOnlyMapper<T> = { readonly [k in keyof T]: T[k] };
+  type ReadOnlyOptions = ReadOnlyMapper<Options>;
+
+  type OptionalMapper<T> = { [k in keyof T]?: T[k] };
+  type OptionalOptions = OptionalMapper<Options>;
+
+  type NullableOptionsMapper<T> = { [k in keyof T]: T[k] | null };
+  type NullableOptions = NullableOptionsMapper<Options>;
+}
+
+{
+  interface Options {
+    subtitles: boolean;
+    volume: number;
+    brightness: number;
+  }
+
+  type ReadOnlyOptions = Readonly<Options>;
+  type OptionalOptions = Partial<Options>;
 }
